ac4717278ecddf4923a97fb39550fa359d995e99
Virak Khun <virak.khun@okoone.com>
2023-09-26 22:30:02 +0700

    chore: update title docs

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home(): JSX.Element {
 	const { siteConfig } = useDocusaurusContext()
 	return (
 		<Layout
-			title={`Home - ${siteConfig.title}`}
+			title={`Home`}
 			description="Description will go into a meta tag in <head />"
 		>
 			<HomepageHeader />
